f971669f819e50a8aca4abda225f02e33bfb2aee
nandu0420 <pheonixnandu226@gmail.com>
2021-09-10 17:34:04 +0530

    modification

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,16 @@
 package clinic.programming.training;
+import java.utils.ArrayList;
+import java.utils.List;
+
 
 public class Application {
+    public void greet(){
+        List<String> greetings=new ArrayList<>();
+        greetings.add("hello");
+        for(String greeting : greetings){
+            System.out.println("Greeting: "+ greeting);
+        }
+    }
     
     public Application() {
         System.out.println ("Inside Application");
@@ -10,5 +20,6 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+        app.greet();
     }
 }
