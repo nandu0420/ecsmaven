97b38d19bee329585172732aefe692c350a2e5d2
nandu0420 <pheonixnandu226@gmail.com>
2021-09-13 12:57:55 +0530

    updates

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,11 +1,17 @@
 package clinic.programming.training;
-import java.utils.ArrayList;
-import java.utils.List;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 
 public class Application {
+    public int countWords(String words){
+        String[] separateWords = StringUtils.split(words, " ");
+        return (separateWords ==null)? 0:separateWords.length;
+    }
     public void greet(){
-        List<String> greetings=new ArrayList<>();
+       List<String> greetings=new ArrayList<>();
         greetings.add("hello");
         for(String greeting : greetings){
             System.out.println("Greeting: "+ greeting);
@@ -21,5 +27,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
         app.greet();
+        int count = app.countWords("I have four words");
+        System.out.println("Word Count: "+count);
     }
 }
